fcf1af56371aa49677ce8aab454704f658bfa0e8
Pavel <work322@gmail.com>
2022-04-17 10:21:33 +0300

    handle post request and send to console

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require ('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/',(req,res)=>{
 res.render('index')
+})
+
+app.post('/',(req,res)=>{
+    // console.log(req.body)
+   const { city } = req.body
+   console.log(city)
+    res.render('index')
 })
 
 app.listen(3000,()=>{
