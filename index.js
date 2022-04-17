9cce2df61d5e83136cac6005ba2a4245391220d6
Pavel <work322@gmail.com>
2022-04-17 09:03:28 +0300

    изучение констант и модуля для работы с файлами

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ Hello world
 I am Grut
 `
 fs.writeFileSync('nodeJs.txt', data)
+
+const result=fs.readFileSync('nodeJs.txt',{encoding: 'utf-8'})
+
+console.log(result)
+
+console.log(__dirname)
+console.log(__filename)
